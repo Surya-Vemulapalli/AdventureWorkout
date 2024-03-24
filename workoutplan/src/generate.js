f5ad770976ca141687af5a7597d1cdbbb1191e7c
Surya Vemulapalli <iciclecrash50@gmail.com>
2024-03-24 01:17:55 -0400

    sorting logic

--- workoutplan/src/generate.js
+++ workoutplan/src/generate.js
@@ -15,6 +15,87 @@ const Generate = () => {
 
     generateButton.addEventListener('click', e => {
         console.log(checkboxCardio.checked)
+        /* checkbox data form:
+          typeChecks = [{choice: "cardio", value: true}, ...]
+          areaChecks = [{choice: "legs", value: <Boolean>, ...}]
+          lengthChecks = [...]
+          equipmentChecks = [...]
+        */
+          const typeChecks = [{choice: "cardio", value: true}, {choice: "strength", value: false }, {choice: "yoga", value: false}, {choice: "interval training", value: true},{choice: "no preference", }]
+          //const = areaChecks = [{choice: "legs", value: <Boolean>, ...}]
+          const lengthChecks = []
+          const equipmentChecks = []
+        // get checkbox data
+        // for each each workout:
+            // for each category:
+                // if workout meets at least one checkbox criteria, add to workout score; else add 0
+        // sort workouts by score from highest to lowest
+        // give links of top 3 to user 
+        // workouts.sort((b - a) => b.score - a.score)
+        const workout = {
+          'url': 'https://docs.google.com/document/d/1bzEiWoql-V8b91ymA2XKMjtM8FOCLpBtvceBqekxoJc/edit',
+          'type': 'cardio',
+          'area': 'legs',
+          'length': 6.1,
+          'equipment': 'weights',
+        }
+        let count = 0;
+        let b = false;
+        let pref = true;
+        for (const check of typeChecks){
+          if(check.value && workout.type === check.choice){
+            b = true;
+          }
+          if (check.value && check.choice === "no preference") {
+            pref = false;
+          }
+
+        }
+        
+        
+        if (b && pref) count++;
+
+        b = false;
+        pref = true;
+        for (const check of areaChecks){
+          if(check.value && workout.area === check.choice){
+            b = true;
+          }
+          if (check.value && check.choice === "no preference") {
+            pref = false;
+          }
+        }
+
+        if(b && pref) count++;
+
+        b = false;
+        pref = true;
+        for (const check of lengthChecks){
+          if(check.value && workout.length === check.choice){
+            b = true;
+          }
+          if (check.value && check.choice === "no preference") {
+            pref = false;
+          }
+        }
+
+        if(b && pref) count++;
+
+        b = false;
+        pref = true;
+        for (const check of equipmentChecks){
+          if(check.value && workout.equipment === check.choice){
+            b = true;
+          }
+          if (check.value && check.choice === "no preference") {
+            pref = false;
+          }
+        }
+
+        if(b && pref) count++;
+
+
+
     })
   }, [])
 
